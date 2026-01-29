6455b59c011af3033675cffeca71ca0b37ecb801
Ryanrzr0091 <rzr0091@auburn.edu>
2026-01-29 11:10:11 -0600

    Add files via upload

---
+++ iOS App 4/SceneDelegate.swift
@@ -0,0 +1,10 @@
+func scene(_ scene: UIScene,
+           willConnectTo session: UISceneSession,
+           options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+
+    let window = UIWindow(windowScene: windowScene)
+    window.rootViewController = MoodViewController()
+    window.makeKeyAndVisible()
+    self.window = window
+}
